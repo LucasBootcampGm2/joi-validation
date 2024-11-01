0baeb05015a7a4249740dcf23cb29dcf5d45aaf5
LucasBootcampGm2 <lucas.berardi@gm2dev.com>
2024-11-01 19:16:11 -0300

    feat(joi): clone userDatabase repo and optimize it with joi features

---
+++ joi/validationSchemas.js
@@ -0,0 +1,25 @@
+import Joi from "joi";
+
+const userSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  role: Joi.string().valid("user", "admin").optional(),
+});
+
+const updateUserSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+});
+
+const changePasswordSchema = Joi.object({
+  oldPassword: Joi.string().min(6).required(),
+  newPassword: Joi.string().min(6).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+export { userSchema, updateUserSchema, changePasswordSchema, loginSchema };
